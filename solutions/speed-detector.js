c73ce901f973349ee7c2151bfc2f214843ab3e09
aowuor <angelaowuor@gmail.com>
2022-11-26 19:00:42 +0300

    Completed

--- solutions/speed-detector.js
+++ solutions/speed-detector.js
@@ -2,6 +2,8 @@
 
 // Write a program that takes the speed of a car as input e.g 80. If the speed is less than 70, it should print “Ok”. Otherwise, for every 5 km/s above the speed limit (70), it should give the driver one demerit point and print the total number of demerit points.
 
+// For example, if the speed is 80, it should print: “Points: 2”. If the driver gets more than 12 points, the function should print: “License suspended”.
+
 
 function carSpeed(speed){
     let points;
@@ -10,7 +12,11 @@ function carSpeed(speed){
         return;
     }else{
         points = Math.floor((speed - 70)/5);
+        if(points > 12){
+            console.log("License suspended")
+            return;
+        }
     }
-    return console.log(`Total demerit points is ${points}`);
+    return console.log(`Points: ${points}`);
 }
-carSpeed(100);
+carSpeed(300);
