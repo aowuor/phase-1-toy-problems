4231ea711d8378f570796807813d2baf533c8b7d
aowuor <angelaowuor@gmail.com>
2022-11-26 18:45:52 +0300

    Completed speed-detector program

--- solutions/grade-gerator.js
+++ solutions/grade-gerator.js
@@ -3,51 +3,50 @@
 // Write a function that prompts the user to input student marks. The input should be between 0 and 100. The output should correspond the correct grade, as shown below: 
 // A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40.
 
-// const marks = 120;
-let input = 120;
-function getUserInput(){
 
-}
-
+let userInput = window.prompt("Enter marks");
 
 //function validating user input
-function validateUser(){
-    if(Number.isInteger(input)){
-        if(input < 0 || input > 100){
-            console.log("invalid number");
-        }else{
-            console.log("Student grade equals")
-        }
-    }else{
-        console.log("Enter a valid number between 0 and 100")
-    }
-    // gradeStudent()
-}
-validateUser()
+// function validateUser(){
+//      if(Number.isInteger(userInput)){
+//           if(userInput < 0 || userInput > 100){
+//                console.log("invalid number");
+//           }else{
+//                console.log("Student grade equals")
+//           }
+//      }else{
+//           console.log("Enter a valid number between 0 and 100")
+//      }
+//      return gradeStudent(userInput);
+//  }
 
 
-//function grading students based on input marks
+// //function grading students based on input marks
+let marks = 20;
 function gradeStudent(){
-    
     let grade;
-
     switch(true){
-        case (marks < 40):
-            grade = "E";
-            break
-        case (marks <= 49):
-            grade = "D";
-            break
-        case (marks <= 59):
-            grade = "c";
-            break
-        case (marks <= 79):
-            grade = "c";
-            break
-    case (marks > 79):
-            grade = "A";
-            break
-
-    }
-    console.log(grade);
+          case (marks < 40):
+               grade = "E";
+               break;
+          case (marks < 40):
+               grade = "E";
+               break;
+          case (marks <= 49):
+               grade = "D";
+               break;
+          case (marks <= 59):
+               grade = "c";
+               break;
+          case (marks <= 79):
+               grade = "c";
+               break;
+          case (marks > 79):
+               grade = "A";
+               break;
+     }
+     
+     console.log(grade);
+     return grade;
 }
+gradeStudent();
--- solutions/speed-detector.js
+++ solutions/speed-detector.js
@@ -1,3 +1,16 @@
 // Challenge 2: Speed Detector (Toy Problem)
 
 // Write a program that takes the speed of a car as input e.g 80. If the speed is less than 70, it should print “Ok”. Otherwise, for every 5 km/s above the speed limit (70), it should give the driver one demerit point and print the total number of demerit points.
+
+
+function carSpeed(speed){
+    let points;
+    if(speed < 70){
+        console.log("Ok");
+        return;
+    }else{
+        points = Math.floor((speed - 70)/5);
+    }
+    return console.log(`Total demerit points is ${points}`);
+}
+carSpeed(100);
