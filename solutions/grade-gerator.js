f1b6f9c1fbcbf4ead514998f2f7895f257c67bdb
aowuor <angelaowuor@gmail.com>
2022-11-27 12:43:43 +0300

    Completed

--- solutions/grade-gerator.js
+++ solutions/grade-gerator.js
@@ -3,8 +3,8 @@
 // Write a function that prompts the user to input student marks. The input should be between 0 and 100. The output should correspond the correct grade, as shown below: 
 // A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40.
 
-
-let userInput = prompt("Enter marks");
+let userInput = 30
+// let userInput = prompt("Enter marks");
 
 //function validating user input
 function validateUser(){
